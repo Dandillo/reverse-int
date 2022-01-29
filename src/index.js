9eaa9e61469d3de4775971efdf1fad8decdd5ce2
Danil <75947542+Dandillo@users.noreply.github.com>
2022-01-30 02:37:24 +0500

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let res = 0;
+    let decs = 0;
+    n = Math.abs(n);
+    while (n > 0) {
+        d = n % 10;
+        res = (res*10) + d;
+        n = parseInt(n / 10);
+    }
+    return res;
+    
 }
+
